d0688b003ab9b39a727d3fbfad76b1acff3eef28
Antonio <antonio.drumondcs@gmail.com>
2024-07-19 19:09:39 -0300

    Lots of progress

--- Source/game.hpp
+++ Source/game.hpp
@@ -105,8 +105,20 @@ class piece{
 		else t = 't';
 		rot = 0;
 		rx = 5;
-		ry = 19;
+		ry = 20;
 		blk = new blocks(t);
+	}
+
+	piece(char nT){
+		t = nT;
+		rot = 0;
+		rx = 5;
+		ry = 20;
+		blk = new blocks(t);
+	}
+
+	~piece(){
+		delete blk;
 	}
 
 };
@@ -114,48 +126,123 @@ class piece{
 class stage{
 	public:
 	bool** map;
-	piece* falling;
-	int* xs; // x and y coords of the falling piece
-	int* ys;
+	piece* falling; // Currently falling piece
+	piece* hold; // for when pressing C and holding a piece
+	int RX;
+	int RY; // Root coords of falling piece
 
 	stage(){
-		map = new bool*[20];
-		for(int i=0; i<20; i++){
+		map = new bool*[21];
+		for(int i=0; i<21; i++){
 			*(map+i) = new bool[10];
 			for(int j=0; j<10; j++){
 				*(*(map+i)+j) = false;
 			}
 		}
-		falling = new piece();
-		xs = new int[4];
-		ys = new int[4];
+		falling = nullptr;
+		hold = nullptr;
 	}
 	
 	~stage(){
 		if(map!=nullptr){
-			for(int i=0; i<20; i++){
+			for(int i=0; i<21; i++){
 				if(*(map+i) != nullptr) delete *(map+i);
 			}
 			delete map;
 		}
-		delete xs;
-		delete ys;
+		if(falling!=nullptr) delete falling;
+		if(hold!=nullptr) delete hold;
 	}
 	
-	void draw(){
-		for(int y=19; y>-1; y--){
+	void drawBoard(){
+		cls();
+		for(int y=20; y>0; y--){
 			for(int x=0; x<10; x++){
-				print(y, x);
+				if(checkDraw(y, x)) print();
+				else print(y, x);
 			}
 			std::cout << "|\n";
 		}
 	}
 
+	void drawStats(){
+		// Stats and scoreboard, held piece, etc
+		// Will need a small board for held piece
+	}
+
+	void newPiece(){ // Spawns a random piece
+		if(falling!=nullptr) delete falling;
+		falling = new piece();
+		RX = 5;
+		RY = 19;
+	}
+
+	void newPiece(char t){ // Spawns a piece of type t
+		if(falling!=nullptr) delete falling;
+		falling = new piece(t);
+		RX = 5;
+		RY = 19;
+	}
+
+	bool fall(){ // Makes the current falling piece go down, if possible
+		if(checkFall()){
+			RY--;
+			return true;
+		}
+		else return false;
+	}
+
+	void place(){
+		map[falling->blk->ys[0]+RY][falling->blk->xs[0]+RX] = true;
+		map[falling->blk->ys[1]+RY][falling->blk->xs[1]+RX] = true;
+		map[falling->blk->ys[2]+RY][falling->blk->xs[2]+RX] = true;
+		map[falling->blk->ys[3]+RY][falling->blk->xs[3]+RX] = true;
+		std::cout << "PLACED\n";
+	}
+
+	void checkLines(){
+
+	}
+
+	void rotLeft(){
+
+	}
+
+	void rotRight(){
+
+	}
+
 	private:
+	bool checkDraw(int y, int x){
+		if(x==(falling->blk->xs[0]+RX) && y==(falling->blk->ys[0]+RY)) return true;
+		else if(x==(falling->blk->xs[1]+RX) && y==(falling->blk->ys[1]+RY)) return true;
+		else if(x==(falling->blk->xs[2]+RX) && y==(falling->blk->ys[2]+RY)) return true;
+		else if(x==(falling->blk->xs[3]+RX) && y==(falling->blk->ys[3]+RY)) return true;
+		else return false;
+	}
+
+	bool checkFall(){
+		// If theres a block beneath
+		if(map[falling->blk->ys[0]+RY-1][falling->blk->xs[0]+RX]) return false;
+		else if(map[falling->blk->ys[1]+RY-1][falling->blk->xs[1]+RX]) return false;
+		else if(map[falling->blk->ys[2]+RY-1][falling->blk->xs[2]+RX]) return false;
+		else if(map[falling->blk->ys[3]+RY-1][falling->blk->xs[3]+RX]) return false;
+		// If hit bottom
+		else if((falling->blk->ys[0]+RY-1)<1) return false;
+		else if((falling->blk->ys[1]+RY-1)<1) return false;
+		else if((falling->blk->ys[2]+RY-1)<1) return false;
+		else if((falling->blk->ys[3]+RY-1)<1) return false;
+		else return true;
+	}
+
 	void print(int y, int x){
 		if(map[y][x])
 			std::cout << "|x";
 		else
 			std::cout << "| ";
+	}
+
+	void print(){
+		std::cout << "|x";
 	}
 };
--- Source/teCris.cpp
+++ Source/teCris.cpp
@@ -4,15 +4,25 @@ bool alive = true;
 
 int main(){
 	std::cout << "Press ENTER to play\n";
-	stage* stg = new stage();
-	getchar();
-	cls();
-	stg->draw();
 	getchar();
 	cls();
 	/*while(alive){	// Game loop
 		cls();
 	}*/
+	int i=0;
+	stage* stg = new stage();
+	while(i<100){
+		if(stg->falling==nullptr) stg->newPiece();
+		bool shldPlace = !stg->fall();
+		if(shldPlace) stg->place();
+		stg->drawBoard();
+		//std::cout << "flag\n"; getchar();
+		if(shldPlace){
+			delete stg->falling;
+			stg->falling = nullptr;
+		}
+		sleep(1*sec);
+	}
 	std::cout << "Press ENTER to exit ";
 	getchar();
 	return 0;
